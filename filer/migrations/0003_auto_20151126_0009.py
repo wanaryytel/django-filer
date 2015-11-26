8b55568a6dc1811ba7bd9a38b2b451d4af65a164
Tadas Dailyda <tadas@dailyda.com>
2015-11-26 20:12:43 +0200

    fix custom migration

--- filer/migrations/0003_auto_20151126_0009.py
+++ filer/migrations/0003_auto_20151126_0009.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('filer', '0001_initial'),
+        ('filer', '0002_auto_20150606_2003'),
     ]
 
     operations = [
